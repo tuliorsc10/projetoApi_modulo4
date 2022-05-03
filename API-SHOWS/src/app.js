3a815b1667f0bddc4f69d934e20f624922a3149d
Damasio <damasioramos90@gmail.com>
2022-05-02 22:23:15 -0300

    importando e chamando o controller

--- API-SHOWS/src/app.js
+++ API-SHOWS/src/app.js
@@ -4,6 +4,9 @@ const app = express();
 app.use(express.json());
 
 const db = require('./infra/sqliteDb');
+const show = require('./showsController/showController');
+
+show(app, db)
 
 app.get('/', (req, res) => {
     res.status(200).send(`tudo ok por enquanto`)
