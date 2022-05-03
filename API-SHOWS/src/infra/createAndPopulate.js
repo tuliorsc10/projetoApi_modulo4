f08f1ce99154d4929e8f1c3479afb0488802ddcd
Damasio <damasioramos90@gmail.com>
2022-05-02 22:29:15 -0300

    reparando erros ao popular a tabela

--- API-SHOWS/src/infra/createAndPopulate.js
+++ API-SHOWS/src/infra/createAndPopulate.js
@@ -5,25 +5,25 @@ const SHOWS_SCHEMA = `
 CREATE TABLE IF NOT EXISTS "SHOWS" (
     "id" INTEGER PRIMARY KEY AUTOINCREMENT,
     "banda" varchar(64),
-    "dia" varchar(64),
+    "data" varchar(64),
     "horario" varchar(64)
 );`;
 
 const ADD_SHOWS_DATA = `
-INSERT INTO SHOWS (id, banda, dia, horario)
+INSERT INTO SHOWS (id, banda, data, horario)
 VALUES
-    (1,'System of a Down','06/05/2022','19:00')
-    (2,'Ira','09/05/2022','19:00')
+    (1,'System of a Down','06/05/2022','19:00'),
+    (2,'Ira','09/05/2022','19:00'),
     (3,'Chitãozinho e Xororó','10/05/2022','19:00')
 `
 
-const criaTabelaShows = () =>{
+const criaTabelaShows = function() {
     db.run(SHOWS_SCHEMA, (error) => {
         if(error) console.log(`Erro ao criar a tabela Shows`);
     });
 }
 
-const populaTabelaShows = () => {
+const populaTabelaShows = function() {
     db.run(ADD_SHOWS_DATA, (error) => {
         if(error) console.log(`Erro ao popular tabela Shows`);
     });
