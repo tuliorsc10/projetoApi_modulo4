657cb6870eaf1abc16997cced960c50e71f9e485
Damasio <damasioramos90@gmail.com>
2022-05-02 22:04:26 -0300

    controller

---
+++ API-SHOWS/src/showsController/showController.js
@@ -0,0 +1,76 @@
+const Show = require('../showsModel/showModel')
+const ShowDAO = require('../DAO/showsDAO')
+
+const show = (app,db) => {
+    const showDAO = new ShowDAO(db)
+
+    app.get('/show', (req, res) =>{
+        const data = async() => {
+            try{
+                const show = await showDAO.listarShows()
+                res.status(200).json(show)
+            }catch(error){
+                res.status(400).json(error)
+            }
+        }
+        data()
+    })
+    
+    app.get('/show/:id', (req, res) => {
+        const body = req.body;
+        const id = req.params.id;
+        const data = async() =>{
+            try{
+                const show = await showDAO.listarShowsPorId(id, body)
+                res.status(200).json(show)
+            }catch(error){
+                res.status(404).json(error)
+            }
+        }
+        data()
+    })
+
+    app.post('/show', (req, res) => {
+        const body = req.body;
+        const data = async() =>{
+            try{
+                const novoShow = new Show(body.banda, body.data, body.horario)
+                const show = await showDAO.insereShow(novoShow)
+                res.status(201).json(show)
+            }catch(error){
+                res.status(400).json(error)
+            }
+        }
+        data()
+    })
+
+    app.put('/show/:id', (req, res) => {
+        const body = req.body;
+        const id = req.params.id
+        const data = async() => {
+            try{
+                const showAlterado = new Show(body.banda, body.data, body.horario)
+                const show = await showDAO.alteraShow(id, showAlterado)
+                res.status(200).json(showAlterado) 
+            }catch(error){
+                res.status(400).json(error)
+            }           
+        }
+        data()
+    })
+
+    app.delete('/show/:id', (req, res) => {
+        const data = async() => {
+            const id = req.params.id
+            try{
+                const show = await showDAO.deletaShow(id)
+                res.status(200).json(show)
+            }catch(error){
+                res.status(400).json(error)
+            }
+        }
+        data()
+    })
+}
+
+module.exports = show
