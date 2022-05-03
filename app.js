bd1b1b623102ce73a3157d025ecc873221d0e70f
Túlio Roberto <96267872+tuliorsc10@users.noreply.github.com>
2022-05-03 20:50:08 -0300

    Entidade Sobremesa Funcionando

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const BurguerConstructor = require("./src/model/ModelHamburguer");
 const PratosPrincipaisConstructor = require("./src/model/ModelPratoPrincipal");
+const SobremesasConstructor = require("./src/model/ModelSobremesa")
 
 const app = express();
 
@@ -8,11 +9,13 @@ app.use(express.json())
 
 const HamburguerController = require('./src/controller/controllerHamburguer')
 const PratoPrincipalController = require('./src/controller/controllerPratoPrincipal')
+const SobremesaController = require('./src/controller/controllerSobremesa')
 
 const bd = require('./src/infra/sqlite-bd')
 
 HamburguerController(app,bd)
 PratoPrincipalController(app,bd)
+SobremesaController(app,bd)
 
 
 
--- src/controller/controllerSobremesa.js
+++ src/controller/controllerSobremesa.js
@@ -1,22 +1,24 @@
 const res = require("express/lib/response")
-const bd = require("../infra/sq-litebd")
+const bd = require("../infra/sqlite-bd")
 const SobremesasConstructor = require("../model/ModelSobremesa")
+const SobremesaDAO = require("../DAO/sobremesaDAO")
 
 const sobremesa = (app,bd) => {
-    app.get('/sobremesa', (req,res) => {
-            bd.all(`SELECT * FROM SOBREMESAS`, (error, rows) =>{
-                if(error) {
-                    res.json("Erro ao selecionar o Banco")
-                } else {
-                    res.json({"Banco sobremesas selecionado" : rows})
-                }
+
+    const instanciaSobremesaDao =  new SobremesaDAO(bd)
+    app.get('/sobremesa', function(req,res) {
+        instanciaSobremesaDao.listarSobremesas()
+        .then((resposta) => {
+            res.json(resposta)
+        }).catch((error) => {
+            res.json(error)
         })
     })
 
-    app.get('/clientes/:id', (req,res) => {
+    app.get('/sobremesa/:id', (req,res) => {
         const requisicao = req.params.id
 
-        bd.all(`SELECT * FROM CLIENTES WHERE id = ${requisicao}`, (error, rows) => {
+        bd.all(`SELECT * FROM SOBREMESAS WHERE id = ${requisicao}`, (error, rows) => {
             if(error) {
                 console.log(typeof requisicao, requisicao)
                 res.json({"Erro ao retornar nome" : error})
@@ -28,12 +30,12 @@ const sobremesa = (app,bd) => {
         })
     })
 
-    app.post('/usuario', (req,res) =>{
+    app.post('/sobremesa', (req,res) =>{
         const add = req.body
         console.log(add)
-        bd.all(`INSERT INTO CLIENTES (NOME,CPF,EMAIL,SENHA) VALUES(?,?,?,?)`, 
-        [add.nome, add.cpf, add.email, add.senha])
-        res.send("foi")
+        bd.all(`INSERT INTO SOBREMESA (ID, TITULO, PRECO, INGREDIENTES) VALUES(?,?,?,?)`, 
+        [add.id, add.titulo, add.preco, add.ingredientes])
+        res.send("Item Sobremesa Cadastrado")
     })
 
 }
