91e963a10363e0763a5732400b66231138afd7e8
diego <diegoed1@gmail.com>
2022-05-09 19:08:46 -0300

    API Charuto atualizando

--- Projeto_M4/api-app/src/controllers/charuto_controller.js
+++ Projeto_M4/api-app/src/controllers/charuto_controller.js
@@ -33,53 +33,74 @@ const InstCharutoDao = new CharutoDAO(bdSqlite)
 
         })
 
-        app.get('/charuto/:nome', (req, res) => {
-            const nome = req.params.nome
-            res.json({
-                "mensagem": " por parametro",
-                "parametro": nome,
-
-            })
-        })
-    
-        app.get('/charuto/:nacionalidade', (req, res) => {
-            const nacionalidade = req.params.nacionalidade
-            for (let i = 0; i <= bdSqlite.length; i++) {
-                if (bdSqlite[i].nacionalidade == nacionalidade) {
-                    return `a nacionalidade encontrada é ${nacionalidade}`
+        
+           
+        app.get('/charuto/:id', (req, res) => {
+            const id = req.params.id
+            const data = async() => {
+                try{
+                    const charutos = await InstCharutoDao.listarCharutosid(id)
+                    res.send(charutos)
+                }catch(err){
+                    res.send(err)
                 }
+        
             }
-        })
+            
+            data()
+            })
+        
 
-        app.put('/charuto/:nacionalidade', (req, res) => {
-        const nacionalidade = req.params.nacionalidade
+
+        app.put('/charuto/:id', (req, res) => {
+        const id = req.params.id
         const body = req.body
-        const indexCharuto = bdSqlite.charuto.findIndex((charuto => charuto.nacionalidade === nacionalidade))
-
-        try {
-            if (indexCharuto > -1) {
-                const charutoAntigo = bdSqlite.charuto[indexCharuto]
+        
+      
+        const data = async() => {
+            try{
+                const CharutosDados = await InstCharutoDao.listarCharutosid(id)                
                 const charutoAtualizado = new Charuto(
-                    body.nome || charutoAntigo.nome,
-                    body.nacionalidade || charutoAntigo.nacionalidade,
-                    body.fornecedor || charutoAntigo.fornecedor,
-                    body.escala_Sabor || charutoAntigo.escala_Sabor,
-                    charutoAntigo.id,
-                )
-                const DadoCharutoAtualizado = bdSqlite.charuto.splice(indexCharuto, 1, charutoAtualizado)
-                res.json({"atualizado": DadoCharutoAtualizado,})
-            } else {
-                res.json({"mensagem": `Charuto com nacionalidade "${nacionalidade}" não existe`,})
+                    body.nome || CharutosDados[0].NOME,
+                    body.nacionalidade || CharutosDados[0].NACIONALIDADE,
+                    body.fornecedor || CharutosDados[0].FORNECEDOR,
+                    body.escalaSabor || CharutosDados[0].ESCALA_SABOR
+                   
+                )               
+
+                const dados = [charutoAtualizado.nome,charutoAtualizado.nacionalidade, charutoAtualizado.fornecedor, charutoAtualizado.escala_Sabor,id]
+                console.log(dados)
+                const charutos = await InstCharutoDao.alteraCharuto(dados)
+                console.log(charuto)
+                res.send(charutos)
+            }catch(err){
+                res.send(err)
             }
-        } catch (error) {
-            res.json({ "mensagem": error.message,})
+    
         }
+        
+        data()
+
+       
     })
 
-    app.delete('/charuto/:nacionalidade', (req, res)=> {
-        const nacionalidade = req.params.nacionalidade
-        const charuto = InstCharutoDao.deletaCharuto(nacionalidade) 
-        res.status(200).json(charuto)
+
+
+    app.delete('/charuto/:id', (req, res)=> {
+            
+        
+            const id = req.params.id
+            const data = async() => {
+                try{
+                    const charutos = await InstCharutoDao.deletaCharuto(id)
+                    res.send(charutos)
+                }catch(err){
+                    res.send(err)
+                }
+        
+            }
+            
+            data()
     })
            
               
